449ce95affe449c23de4cc47483097102bf68357
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-11 12:24:28 +0100

    Switch to rocket ignite and nightly buildchain

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
 #[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
+fn hello(name: String, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+fn main(){
+    rocket::ignite()
+    .mount("/", routes![hello])
+    .launch();
 }
